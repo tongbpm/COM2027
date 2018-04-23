c97bb33462a60210a52a42250770653185634746
Alex Potter <alexpotter1@icloud.com>
2018-04-23 14:00:14 +0100

    SynchronisationManager: Add firebase remote path lookup

--- app/src/main/java/org/com2027/group11/beerhere/utilities/database/SynchronisationManager.java
+++ app/src/main/java/org/com2027/group11/beerhere/utilities/database/SynchronisationManager.java
@@ -1,11 +1,29 @@
 package org.com2027.group11.beerhere.utilities.database;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Created by apotter on 23/04/18.
  */
 public class SynchronisationManager {
 
     private static SynchronisationManager instance = null;
+    private final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    private HashMap<String, String> firebasePaths = new HashMap<String, String>() {
+        {
+            put("beer", "server/beers");
+            put("user", "server/users");
+        }
+    };
+
     protected SynchronisationManager() {}
 
     public static SynchronisationManager getInstance() {
@@ -14,5 +32,32 @@ public class SynchronisationManager {
         }
         return instance;
     }
+
+    @Nullable
+    private String searchForFirebasePath(@NonNull String criterion) {
+        // Search by key
+        Iterator iter = this.firebasePaths.entrySet().iterator();
+
+        String foundPath = null;
+
+        while (iter.hasNext()) {
+            Map.Entry pair = (Map.Entry) iter.next();
+            if (pair.getKey().toString().toUpperCase().equals(criterion.toUpperCase())) {
+                foundPath = pair.getValue().toString();
+            }
+        }
+
+        // Return null if the path is not found...this needs to be handled later on
+        if (foundPath != null) {
+            return foundPath;
+        }
+        return null;
+    }
+
+    public boolean saveBeerToFirebase() {
+        return true;
+    }
+
+
 
 }
