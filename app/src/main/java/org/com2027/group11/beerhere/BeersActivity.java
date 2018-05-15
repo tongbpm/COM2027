51191a1e6b4df2cbd34a7961763e46057409bbb9
xJ3rian <33238592+xJ3rian@users.noreply.github.com>
2018-05-15 15:21:37 +0100

    Added Navigation Drawer
    Added Basic Navigation Drawer
    -Currently has no functionalities
    -Opens the drawer when the button is tapped or when it's slided out
    -Click events and navigation to be added

--- app/src/main/java/org/com2027/group11/beerhere/BeersActivity.java
+++ app/src/main/java/org/com2027/group11/beerhere/BeersActivity.java
@@ -2,11 +2,17 @@ package org.com2027.group11.beerhere;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -20,13 +26,31 @@ public class BeersActivity extends AppCompatActivity {
 
     private RecyclerView rvBeers;
     private BeerListAdapter adapter;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beers_page);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    //set item as selected to persist highlight
+                    menuItem.setChecked(true);
+                    //close drawer when item is tapped
+                    mDrawerLayout.closeDrawers();
+                    return false;
+                }
+        );
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +62,16 @@ public class BeersActivity extends AppCompatActivity {
         });
 
         displayBeers();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void displayBeers(){
