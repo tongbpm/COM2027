ba1ed98fc2a1ed5846f119cf8831bd991934191b
River Phillips <riverphillips1@gmail.com>
2018-05-24 17:57:25 +0100

    Updated beer test for new constructor
    Signed-off-by: River Phillips <riverphillips1@gmail.com>

--- app/src/test/java/org/com2027/group11/beerhere/BeerTest.java
+++ app/src/test/java/org/com2027/group11/beerhere/BeerTest.java
@@ -3,6 +3,8 @@ package org.com2027.group11.beerhere;
 import org.junit.Test;
 import org.com2027.group11.beerhere.beer.Beer;
 
+import java.util.HashSet;
+
 import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
@@ -24,7 +26,7 @@ public class BeerTest {
         String imageid = "1234";
         int upvotes = 350;
         int downvotes = 0;
-        Beer beer = new Beer(name, imageid, upvotes, downvotes);
+        Beer beer = new Beer(name, imageid, upvotes, downvotes,"1",10,10, new HashSet<String>(), new HashSet<String>());
         assertEquals(beer.name, name);
         assertEquals(beer.imageID, imageid);
         assertEquals(beer.upvotes,  upvotes);
